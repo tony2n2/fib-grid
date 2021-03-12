9de31dfc6ca14c0e433a5df2b36668667593d4fb
Tony Nan <tony2n2@users.noreply.github.com>
2021-03-12 22:51:38 +0100

    store fib sequence with array instead of objects

--- src/FibSequence.js
+++ src/FibSequence.js
@@ -3,25 +3,33 @@
  */
 class FibSequence {
 	constructor() {
+		this.fibSeq = [0, 1, 2];
 		this.fibs = Object.create(null);
-		this.fibs[0] = { prev: null, next: 1 };
-		this.fibs[1] = { prev: 0, next: 2 };
+		this.fibs[0] = 0;
+		this.fibs[1] = 1;
 		// because keys are unique we have to skip the second '1' in the sequence
-		this.fibs[2] = { prev: 1, next: 3 };
-		this.tail = 2;
+		this.fibs[2] = 2;
 	}
 
-	getNumber(n) {
+	getNeightboors(n) {
 		n = parseInt(n);
 		// TODO: if n === NaN
 
 		// extend the cached sequence if necessary
-		while (this.tail < n) {
-			const tailNode = this.fibs[this.tail];
-			this.fibs[tailNode.next] = { prev: this.tail, next: this.tail + tailNode.next };
-			this.tail = tailNode.next;
+		let prevOfLast = this.fibSeq[this.fibSeq.length - 2];
+		while (prevOfLast < n) {
+			const last = this.fibSeq[this.fibSeq.length - 1];
+			const next = prevOfLast + last;
+			this.fibSeq.push(next);
+			this.fibs[next] = this.fibSeq.length - 1;
+			prevOfLast = this.fibSeq[this.fibSeq.length - 2];
 		}
-		return this.fibs[n];
+
+		const index = this.fibs[n];
+		if (index !== undefined) {
+			return { prev: index !== 0 ? this.fibSeq[index - 1] : null, next: this.fibSeq[index + 1] };
+		}
+		return null;
 	}
 }
 
--- src/FibSequence.test.js
+++ src/FibSequence.test.js
@@ -7,9 +7,9 @@ test('create a new fibonacci sequence', () => {
 
 test('get some number from the sequence', () => {
 	const seq = new FibSequence();
-	expect(seq.getNumber(8)).toEqual({ prev: 5, next: 13 });
-	expect(seq.getNumber(55)).toEqual({ prev: 34, next: 89 });
-	expect(seq.getNumber(21)).toEqual({ prev: 13, next: 34 });
-	expect(seq.getNumber(3)).toEqual({ prev: 2, next: 5 });
-	expect(seq.getNumber(144)).toEqual({ prev: 89, next: 233 });
+	expect(seq.getNeightboors(8)).toEqual({ prev: 5, next: 13 });
+	expect(seq.getNeightboors(55)).toEqual({ prev: 34, next: 89 });
+	expect(seq.getNeightboors(21)).toEqual({ prev: 13, next: 34 });
+	expect(seq.getNeightboors(3)).toEqual({ prev: 2, next: 5 });
+	expect(seq.getNeightboors(144)).toEqual({ prev: 89, next: 233 });
 });
