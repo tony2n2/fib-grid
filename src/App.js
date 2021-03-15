b87262c1edd7dfd4e2443aae2f7464568fc3139d
Tony Nan <tony2n2@users.noreply.github.com>
2021-03-15 16:57:01 +0100

    change backgroundColor duuring increments & clearance

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import FibGrid from './FibGrid';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const ROWS = 50;
 const COLUMNS = 50;
@@ -8,13 +8,19 @@ const fg = new FibGrid(ROWS, COLUMNS);
 
 function App() {
 	const [state, setState] = useState(fg.grid);
+	const timer = useRef(null);
 	function clickHandler(e) {
 		fg.incrementRowCol(e.target.attributes.row.value, e.target.attributes.col.value);
-		const fibs = fg.matchFibOnRowCol(e.target.attributes.row.value, e.target.attributes.col.value);
-		fibs.forEach((seq) => {
-			fg.resetRange(seq);
-		});
 		setState(fg.grid);
+		const fibs = fg.matchFibOnRowCol(e.target.attributes.row.value, e.target.attributes.col.value);
+    // reset matched cells to empty after 1s
+		timer.current = setTimeout(() => {
+			fibs.forEach((seq) => {
+				fg.resetRange(seq);
+			});
+			setState(fg.grid);
+			timer.current = null;
+		}, 1000);
 	}
 	return (
 		<div className="App">
@@ -39,11 +45,45 @@ function App() {
 }
 
 function Cell(props) {
+	const timer = useRef(null);
+	const initialRun = useRef(true);
+
+	// background color
+	const [backgroundColor, setBackgroundColor] = useState('');
+	useEffect(() => {
+		if (initialRun.current) {
+			initialRun.current = false;
+			return;
+		}
+		if (timer.current) {
+			clearTimeout(timer.current);
+		}
+		setBackgroundColor(props.value === 0 ? 'green' : 'yellow');
+		timer.current = setTimeout(() => {
+			setBackgroundColor('');
+			timer.current = null;
+		}, 1000);
+	}, [props.value]);
+
+	// cleanup when unmount
+	useEffect(() => {
+		return () => {
+			if (timer.current) {
+				clearTimeout(timer.current);
+			}
+		};
+	}, []);
+
 	// cell width css prop
 	let width = { width: `calc((100vmin - 40px) / ${ROWS})` };
 
 	return (
-		<span className="cell" onClick={props.onClick} style={width} {...props.attrs}>
+		<span
+			className="cell"
+			onClick={props.onClick}
+			style={backgroundColor ? { backgroundColor, ...width } : { ...width }}
+			{...props.attrs}
+		>
 			{props.value !== 0 ? props.value : ''}
 		</span>
 	);
